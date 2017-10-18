c65ddd62f92692fde4e1a0e939c10cfd427830fb
JohnFunk-Pivotal <jfunk@pivotal.io>
2017-10-17 22:10:36 -0600

    Beefed up error handling for null or 0 data

--- cleaninput/cleaninput.py
+++ cleaninput/cleaninput.py
@@ -51,25 +51,35 @@ def clean_all_input_errors(inputDataFrame, errorLogFile):
     import re
     compiledRegex=re.compile('\d+')
     for index, row in cleanDataFrame.iterrows():
-        rawWeightString=row['Competitor\'s Weight (eg. 73lbs.)?']
-        matchList=compiledRegex.match(rawWeightString)
-        cleanWeightString=matchList.group()
-#        cleanDataFrame.loc[index,'Competitor\'s Weight (in lbs.)?']=cleanWeightString #try .at instead http://pandas.pydata.org/pandas-docs/stable/indexing.html#fast-scalar-value-getting-and-setting
-        cleanDataFrame.at[index,'Competitor\'s Weight (eg. 73lbs.)?']=cleanWeightString #try .at instead http://pandas.pydata.org/pandas-docs/stable/indexing.html#fast-scalar-value-getting-and-setting
-
-        #print cleanWeightString
-        cleanWeight=int(cleanWeightString)
-        if (0==cleanWeight) or (350 < cleanWeight):
-            errorCount+=1
-            errorString="Error: The row: "+str(row["Registrant ID"])+" "+str(row["First Name"])+" "+str(row["Last Name"])+ " has an invalid Weight field:" + rawWeightString
-            errorLogFile.write(errorString+"\r\f")
+        rawWeightString=row['Competitor\'s Weight (e.g. 73lbs.)?']
+        if pd.isnull(rawWeightString):
+            errorCount += 1
+            errorString = "Error: The row: " + str(row["Registrant ID"]) + " " + str(row["First Name"]) + " " + str(
+                row["Last Name"]) + " has an empty Weight field"
+            errorLogFile.write(errorString + "\r\f")
             print errorString
+        else:
+            matchList=compiledRegex.match(rawWeightString)
+            if matchList is None:
+                cleanWeightString='0'
+            else:
+                cleanWeightString=matchList.group()
+    #        cleanDataFrame.loc[index,'Competitor\'s Weight (in lbs.)?']=cleanWeightString #try .at instead http://pandas.pydata.org/pandas-docs/stable/indexing.html#fast-scalar-value-getting-and-setting
+            cleanDataFrame.at[index,'Competitor\'s Weight (e.g. 73lbs.)?']=cleanWeightString #try .at instead http://pandas.pydata.org/pandas-docs/stable/indexing.html#fast-scalar-value-getting-and-setting
+
+            #print cleanWeightString
+            cleanWeight=int(cleanWeightString)
+            if (0==cleanWeight) or (350 < cleanWeight):
+                errorCount+=1
+                errorString="Error: The row: "+str(row["Registrant ID"])+" "+str(row["First Name"])+" "+str(row["Last Name"])+ " has an invalid Weight field:" + rawWeightString
+                errorLogFile.write(errorString+"\r\f")
+                print errorString
 
 
 #    #Check for non-numeric data in the Weight Field
 #    for index, row in cleanDataFrame.iterrows():
 #        try:
-#            int(row['Competitor\'s Weight (eg. 73lbs.)?'])
+#            int(row['Competitor\'s Weight (e.g. 73lbs.)?'])
 #        except ValueError:
 #            errorCount+=1
 #            errorString="Error: The row: "+str(row["Registrant ID"])+" "+str(row["First Name"])+" "+str(row["Last Name"])+ " has something other than a number in Weight field"
@@ -89,53 +99,58 @@ def clean_all_input_errors(inputDataFrame, errorLogFile):
     compiledRegex=re.compile('\d+')
     for index, row in cleanDataFrame.iterrows():
         splitString=row['Competitor\'s Height (e.g. 4 ft. 2 in. )?']
- #        print splitString
-        matchList=compiledRegex.findall(splitString)
-        l=len(matchList)
-        if l>=2:
-            feet=matchList[0]
-            inches=matchList[1]
-        if l==1:
-            if(int(matchList[0])<12):
-                #single digit less than 12
+        if pd.isnull(splitString):
+            errorCount += 1
+            errorString = "Error: The row: " + str(row["Registrant ID"]) + " " + str(row["First Name"]) + " " + str(
+                row["Last Name"]) + " has an empty Height field"
+            errorLogFile.write(errorString + "\r\f")
+            print errorString
+        else:
+            matchList=compiledRegex.findall(splitString)
+            l=len(matchList)
+            if l>=2:
                 feet=matchList[0]
+                inches=matchList[1]
+            if l==1:
+                if(int(matchList[0])<12):
+                    #single digit less than 12
+                    feet=matchList[0]
+                    inches=0
+                else:
+                    #single digit greater than 12
+                    feet= 0
+                    inches=matchList[0]
+                    if inches>12:
+                        feet=int(inches)/12
+                        inches=int(inches)%12
+            if l==0:
+                feet=0
                 inches=0
-            else:
-                #single digit greater than 12
-                feet= 0
-                inches=matchList[0]
-                if inches>12:
-                    feet=int(inches)/12
-                    inches=int(inches)%12
-        if l==0:
-            feet=0
-            inches=0
 
-        #resonability test
-        if (int(feet) < 2) or (int(feet) > 7):
-           #print splitString, "|", feet, "|", inches, "is not reasonable"
-           errorCount+=1
-           errorString="Error: The row: "+str(row["Registrant ID"])+" "+str(row["First Name"])+" "+str(row["Last Name"])+ " has bad data in the height field: " + splitString
-           errorLogFile.write(errorString+"\r\f")
-           print errorString
+            #resonability test
+            if (int(feet) < 2) or (int(feet) > 7):
+               #print splitString, "|", feet, "|", inches, "is not reasonable"
+               errorCount+=1
+               errorString="Error: The row: "+str(row["Registrant ID"])+" "+str(row["First Name"])+" "+str(row["Last Name"])+ " has bad data in the height field: " + splitString
+               errorLogFile.write(errorString+"\r\f")
+               print errorString
 
-        #Write it to new columns in the data frame
-        cleanDataFrame.loc[index,'Feet']=feet
-        cleanDataFrame.loc[index,'Inches']=inches
-        heightInInches = (int(feet)*12)+int(inches)
-        cleanDataFrame.loc[index,'HeightInInches']=heightInInches
-        bodyMassIndex=(heightInInches*2)+int(cleanDataFrame.loc[index,'Competitor\'s Weight (eg. 73lbs.)?'])
-        cleanDataFrame.loc[index,'BMI']=bodyMassIndex
-        #print splitString, "|", feet, "|", inches
+            #Write it to new columns in the data frame
+            cleanDataFrame.loc[index,'Feet']=feet
+            cleanDataFrame.loc[index,'Inches']=inches
+            heightInInches = (int(feet)*12)+int(inches)
+            cleanDataFrame.loc[index,'HeightInInches']=heightInInches
+            bodyMassIndex=(heightInInches*2)+int(cleanDataFrame.loc[index,'Competitor\'s Weight (e.g. 73lbs.)?'])
+            cleanDataFrame.loc[index,'BMI']=bodyMassIndex
+            #print splitString, "|", feet, "|", inches
 
-#---test version
-        cleanDataFrame.at[index,'Feet']=feet
-        cleanDataFrame.at[index,'Inches']=inches
-        heightInInches = (int(feet)*12)+int(inches)
-        cleanDataFrame.at[index,'HeightInInches']=heightInInches
-        bodyMassIndex=(heightInInches*2)+int(cleanDataFrame.loc[index,'Competitor\'s Weight (eg. 73lbs.)?'])
-        cleanDataFrame.at[index,'BMI']=bodyMassIndex
-        #print splitString, "|", feet, "|", inches
+            cleanDataFrame.at[index,'Feet']=feet
+            cleanDataFrame.at[index,'Inches']=inches
+            heightInInches = (int(feet)*12)+int(inches)
+            cleanDataFrame.at[index,'HeightInInches']=heightInInches
+            bodyMassIndex=(heightInInches*2)+int(cleanDataFrame.loc[index,'Competitor\'s Weight (e.g. 73lbs.)?'])
+            cleanDataFrame.at[index,'BMI']=bodyMassIndex
+            #print splitString, "|", feet, "|", inches
 
     #Look for out of state dojos and move them into the 'Select Your Z Ultimate Studio' column
     print "  " + time.strftime("%X") + " Looking for out of state dojos"

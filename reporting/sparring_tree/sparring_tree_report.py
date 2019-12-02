d2bd8a6b900c846029d044aacab548a3b6164426
JohnFunkCode <email@johnfunk.com>
2019-12-01 22:47:48 -0700

    Added initial sparring_tree_report code The first iteration of the sparring_tree_report code, and a simple test to test it.

--- reporting/sparring_tree/sparring_tree_report.py
+++ reporting/sparring_tree/sparring_tree_report.py
@@ -1,4 +1,11 @@
+'''
+Sparring_Tree_Report
+Contains code to create a report containing all the sparring trees for a tournament
+'''
+import unittest
 import datetime
+import os
+from reportlab.pdfgen import canvas
 
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter, landscape
@@ -9,11 +16,57 @@ from reportlab.platypus import PageBreak
 from reportlab.platypus import SimpleDocTemplate, Spacer, Table, TableStyle
 # from reportlab.rl_config import defaultPageSize
 
+from reporting.sparring_tree.competitors import Competitors
+from reporting.sparring_tree.eight_competitor_tree import EightCompetitorTree
+
+SPARRING_TREE_REPORT_FILE_NAME = "SparringTreeReport.pdf"
 
 class SparringTreeReportPDF(object):
+    ''' Class to Create a Sparring Tree Report for each sparring event in the tournament '''
+
     def __init__(self):
-        self.somvariable = 2
+        ''' Create a Sparring Tree'''
+        self._canvas = canvas.Canvas(SPARRING_TREE_REPORT_FILE_NAME)
+
+
+    def __del__(self):
+        self._canvas.save()
+
+
+# We want to keep the interface similar to the way the other reports are
+# if created:
+#     Below is an example
+# WritePattern6ToDetailReport(16, "9:00am", "Boy's Sparring", "9-11", compositMask)
+# an example of the mask is: compositMask=mask_Sparring & mask_Age9to11 & mask_Male
+#
+# a nearly exact copy would be
+# WritePattern6toSparringReport(list_of_rings, "TimeString", "Title", "Division Name", competitors, "competitors selection criteria")
+
+    def write_event_to_sparring_report_using_pattern_1(self, event_time: str, event_title, event_competitors : Competitors):
+        ''' write all the competitors in this event to the sparring tree report using output pattern 1
+            The Pattern it writes is:
+            White
+            Yellow
+            Orange
+            Purple, Blue, Blue Stripe
+            Green, Green Stripe
+         '''
+        tree = EightCompetitorTree(self._canvas)
+        tree.draw_static_template()
+
+
+        #white belts are the first division for this report
+        division_competitors = event_competitors.query( 'Rank == "Yellow"')
+
+        # lay down the template
+        tree.draw_static_template()
+
+        # draw the competitors onto the tree
+        tree.draw_competitors_on_tree(division_competitors)
+
+        tree.close()
 
 
 if __name__ == '__main__':
-    '''test getting the number of SparringTree Report '''
+    '''test the SparringTree Report '''
+
--- reporting/sparring_tree/sparring_tree_report_test.py
+++ reporting/sparring_tree/sparring_tree_report_test.py
@@ -10,56 +10,35 @@ from reportlab.pdfgen import canvas
 
 from reporting.sparring_tree import competitors
 from reporting.sparring_tree.eight_competitor_tree import EightCompetitorTree
+from reporting.sparring_tree.sparring_tree_report import SparringTreeReportPDF
 
 REMOVE_TEST_FILES = False
+
+TEST_DATA_COLUMNS = ['index', 'First Name', 'Last Name', 'Gender', 'Dojo', 'Age', 'Rank', 'Feet', 'Inches', 'Height',
+                     'Weight', 'BMI', 'Events', 'Weapons', 'hitcount']
+TEST_DATA = [(1, 'Katie', 'Coleson', 'Female', 'CO- Parker', 12, 'White', 4, 0, '4', 65, 161,
+              '2 Events - Forms & Sparring ($75)', 'Weapons ($35)', 0),
+             (2, 'Lucas', 'May', 'Male', 'CO- Cheyenne Mountain', 10, 'Yellow', 4, 3, '4 ft. 3 in.', 52, 154,
+              '2 Events - Forms & Sparring ($75)', 'None', 0),
+             (3, 'Jake', 'Coleson', 'Male', 'CO- Cheyenne Mountain', 10, 'Yellow', 4, 0, '4', 60, 156,
+              '2 Events - Forms & Sparring ($75)', 'Weapons ($35)', 0),
+             (4, 'Allen', 'Whitaker', 'Male', 'CO- Arvada', 10, 'Yellow', 4, 0, '4', 55, 151,
+              '2 Events - Forms & Sparring ($75)', 'Weapons ($35)', 0),
+             (5, 'Bill', 'Kable', 'Male', 'CO- Cheyenne Mountain', 10, 'Yellow', 4, 1, '4', 63, 161,
+              '2 Events - Forms & Sparring ($75)', 'Weapons ($35)', 0)]
 
 class TestSparringTreeReport(unittest.TestCase):
     ''' class to test the SparringTreeReport code'''
 
     def setUp(self):
         ''' setup data for the tests'''
-        self._cols = ['index', 'First Name', 'Last Name', 'Gender', 'Dojo', 'Age', 'Rank', 'Feet', 'Inches', 'Height',
-                      'Weight', 'BMI', 'Events', 'Weapons', 'hitcount']
-        self._data = [(1, 'Katie', 'Coleson', 'Female', 'CO- Parker', 12, 'White', 4, 0, '4', 65, 161,
-                       '2 Events - Forms & Sparring ($75)', 'Weapons ($35)', 0),
-                      (2, 'Lucas', 'May', 'Male', 'CO- Cheyenne Mountain', 10, 'Yellow', 4, 3, '4 ft. 3 in.', 52, 154,
-                       '2 Events - Forms & Sparring ($75)', 'None', 0),
-                      (3, 'Jake', 'Coleson', 'Male', 'CO- Cheyenne Mountain', 10, 'Yellow', 4, 0, '4', 60, 156,
-                       '2 Events - Forms & Sparring ($75)', 'Weapons ($35)', 0),
-                      (4, 'Allen', 'Whitaker', 'Male', 'CO- Arvada', 10, 'Yellow', 4, 0, '4', 55, 151,
-                       '2 Events - Forms & Sparring ($75)', 'Weapons ($35)', 0),
-                      (5, 'Bill', 'Kable', 'Male', 'CO- Cheyenne Mountain', 10, 'Yellow', 4, 1, '4', 63, 161,
-                       '2 Events - Forms & Sparring ($75)', 'Weapons ($35)', 0)]
 
 
-
-    def test_draw_competitors_on_tree(self):
-        ''' tests that we each compettitor gets assigned physical coordinates '''
-        #create compettitors from our test data
-        #get the first and second column test coordinates from the tree
-        #run the algorythm to map compettitors into the tree
-        # description of how to add a new column to an existing dataframe https://www.geeksforgeeks.org/adding-new-column-to-existing-dataframe-in-pandas/
-        the_competitors = competitors.Competitors(self._data, columns=self._cols)  # create a list of competitors from the test data above
-
-        # setup an 8 person tree
-        test_file_name = "8PersonTree.pdf"
-        test_canvas = canvas.Canvas(test_file_name)
-        tree = EightCompetitorTree(test_canvas)
-        tree.draw_static_template()
-
-
-        # draw the competitors onto the tree
-        tree.draw_competitors_on_tree(the_competitors)
-
-        # close out the 8 person tree
-        tree.close()
-        test_canvas.save()
-
-        # test to see if the PDF file was created
-        self.assertTrue(os.path.exists(test_file_name))
-        if REMOVE_TEST_FILES:
-            os.remove(test_file_name)
-
+    def test_write_event_to_sparring_report_using_pattern_1(self):
+        the_competitors = competitors.Competitors(TEST_DATA, columns=TEST_DATA_COLUMNS)  # create a list of competitors from the test data above
+        sparring_tree_report = SparringTreeReportPDF()
+        sparring_tree_report.write_event_to_sparring_report_using_pattern_1("9:00am","Kids Kata", the_competitors)
+        del sparring_tree_report
 
 
 if __name__ == '__main__':

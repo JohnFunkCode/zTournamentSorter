b7516dbcf8bceaf9f028d425f745ebdce18e814c
JohnFunkCode <email@johnfunk.com>
2019-12-10 22:13:47 -0700

    Initial Implementation of sixteen competitor tree and it's tests.

---
+++ reporting/sparring_tree/sixteen_competitor_tree.py
@@ -0,0 +1,217 @@
+""" this module contains code to create a 16 person sparring tree"""
+
+from reportlab.lib.pagesizes import letter, landscape
+from reportlab.lib.units import cm
+
+from reportlab.pdfgen import canvas
+
+from reporting.sparring_tree import bracket_position_map as BPM
+from reporting.sparring_tree.competitors import Competitors
+
+
+class SixteenCompetitorTree():
+    """ Creates a 16 compettitor sparring tree"""
+
+    # setup a couple of constants for this tree
+    _OFFSET_BETWEEN_BRANCHES_ON_TREE = 2.1
+    _SPACE_BELOW_TEXT = 0.1  # centimeters
+
+    _c = None
+    _path = None
+
+    _first_column_text_coordinates = None
+    _second_column_text_coordinates = None
+
+    def __init__(self, the_canvas):
+        """ sets up instance variables for this tree """
+        self._c = the_canvas
+        self._c.setPageSize(landscape(letter))  # defaults to 72 pointer per inch
+        self._path = self._c.beginPath()
+        # print(self._c.__dict__)
+        # print('SixteenCompetitorTree initialized with filename:' + self._c.__dict__['_filename'])
+        self.initialize_text_coordinates()
+
+    def close(self):
+        """ generate the page"""
+        self._c.drawPath(self._path, stroke=1, fill=0)
+        self._c.showPage()
+
+    def truncate(self, number, decimal_places=0):
+        ''' utility funciton to truncate a number to a given number of decimal places'''
+        multiplier = 10 ** decimal_places
+        return int(number * multiplier) / multiplier
+
+    def initialize_text_coordinates(self):
+        '''initialize the text coordinates, two columns of x,y coordinate of where names gets drawn'''
+
+        # calculate first column text coordinates top of the page to the bottom
+        self._first_column_text_coordinates = []
+        initial_first_column_text_coords = [[.8, 17.3 + self._SPACE_BELOW_TEXT],
+                                            [.8, 16.2 + self._SPACE_BELOW_TEXT]]
+        for i in range(8):
+            offset = self.truncate(i * self._OFFSET_BETWEEN_BRANCHES_ON_TREE, 1)
+            self._first_column_text_coordinates.append([initial_first_column_text_coords[0][0],
+                                                        self.truncate(initial_first_column_text_coords[0][1] - offset,
+                                                                      1)])
+            self._first_column_text_coordinates.append([initial_first_column_text_coords[1][0],
+                                                        self.truncate(initial_first_column_text_coords[1][1] - offset,
+                                                                      1)])
+
+        self._second_column_text_coordinates = []
+        initial_second_column_text_coords = [7.2, 16.8 + self._SPACE_BELOW_TEXT]
+        for i in range(8):
+            offset = self.truncate(i * self._OFFSET_BETWEEN_BRANCHES_ON_TREE, 1)
+            self._second_column_text_coordinates.append(
+                [initial_second_column_text_coords[0], self.truncate(initial_second_column_text_coords[1] - offset, 1)])
+        i = 0
+
+    def get_canvas_coord_for_nth_competitor_in_column1(self, competitor_index):
+        '''returns the x,y coordinates on the canvas for the given competitor index
+        for example the 2nd compettitor in column 1 will be placed at x,y on the canvas '''
+        px, py = self._first_column_text_coordinates[competitor_index]
+        return self._first_column_text_coordinates[competitor_index]
+
+    def get_canvas_coord_for_nth_competitor_in_column2(self, competitor_index):
+        '''returns the x,y coordinates on the canvas for the given competitor index
+        for example the 2nd compettitor in column 2 will be placed at x,y on the canvas '''
+        return self._second_column_text_coordinates[competitor_index]
+
+    def draw_box(self, left, top):
+        ''' draw a single checkbox at the coordinates provided '''
+        top = top + 1
+        self._path.moveTo(left * cm, top * cm)
+        self._path.lineTo((left - .2) * cm, (top * cm))
+        self._path.lineTo((left - .2) * cm, (top - .2) * cm)
+        self._path.lineTo(left * cm, (top - .2) * cm)
+        self._path.lineTo(left * cm, top * cm)
+
+    def draw_boxes(self, left, top):
+        ''' draw checkboxes at the coordinates provides to keep track of scores '''
+        self.draw_box(left, top)
+        self.draw_box(left + .3, top)
+        self.draw_box(left + .6, top)
+
+    def draw_static_template(self):  # TBD - paused here!!!!
+        """ Draws the static template portion of the tree"""
+
+        # first bracket
+        for i in range(8):
+            offset = i * 2.1
+            self._path.moveTo(.8 * cm, (1.5 + offset) * cm)  # .8 centimeters to the right, (1 + offset) centimeters up
+            self._path.lineTo(6.2 * cm, (1.5 + offset) * cm)
+            self._path.lineTo(7.1 * cm, (2 + offset) * cm)
+            self._path.lineTo(6.2 * cm, (2.6 + offset) * cm)
+            self._path.lineTo(.8 * cm, (2.6 + offset) * cm)
+            self.draw_boxes(1.5, 1.5 + offset)
+            self.draw_boxes(1.5, (2.5 + offset))
+
+        # second bracket
+        for i in range(4):
+            offset = i * 4.2
+            self._path.moveTo(7.1 * cm, (2 + offset) * cm)  # 7.2 centimeters to the right, (2 + offset) centimeters up
+            self._path.lineTo(12.6 * cm, (2 + offset) * cm)
+            self._path.lineTo(13.3 * cm, (3.1 + offset) * cm)
+            self._path.lineTo(12.6 * cm, (4.1 + offset) * cm)
+            self._path.lineTo(7.1 * cm, (4.1 + offset) * cm)
+            self.draw_boxes(7.5, 2 + offset)
+            self.draw_boxes(7.5, 4.1 + offset)
+
+        # third bracket
+        for i in range(2):
+            offset = i * 8.4
+            self._path.moveTo(13.3 * cm,
+                              (3.1 + offset) * cm)  # 13.3 centimeters to the right, (3.1 + offset) centimeters up
+            self._path.lineTo(18.7 * cm, (3.1 + offset) * cm)
+            self._path.lineTo(20.1 * cm, (5.2 + offset) * cm)
+            self._path.lineTo(18.7 * cm, (7.3 + offset) * cm)
+            self._path.lineTo(13.3 * cm, (7.3 + offset) * cm)
+            self.draw_boxes(14, 3.1 + offset)
+            self.draw_boxes(14, 7.3 + offset)
+
+        # fourth bracket
+        offset = 0
+        self._path.moveTo(20.1 * cm, 5.2 * cm)  # 20.1 centimeters to the right, 5.2 centimeters up
+        self._path.lineTo(25.5 * cm, 5.2 * cm)
+        self._path.lineTo(27 * cm, 9.4 * cm)
+        self._path.lineTo(25.5 * cm, 13.6 * cm)
+        self._path.lineTo(20.1 * cm, 13.6 * cm)
+        self.draw_boxes(21, 5.2)
+        self.draw_boxes(21, 13.6)
+
+        # winner line
+        self._path.moveTo(20.1 * cm, 9.4 * cm)  # 20.1 centimeters to the right, 9.4 centimeters up
+        self._path.lineTo(27 * cm, 9.4 * cm)
+
+    def draw_header_info_on_tree(self, ring: int, event_time: str, event_title: str, ranks: str):
+        ''' draw the header text onto the tree '''
+        self._c.drawString(13 * cm, 26.5 * cm, "Time:")
+        self._c.drawString(14.5 * cm, 26.5 * cm, event_time)
+        self._c.drawString(13 * cm, 25.75 * cm, "Event:")
+        self._c.drawString(14.5 * cm, 25.75 * cm, event_title)
+        self._c.drawString(13 * cm, 25 * cm, "Rank:")
+        self._c.drawString(14.5 * cm, 25 * cm, ranks)
+        self._c.drawString(13 * cm, 24.25 * cm, "Ring#:")
+        self._c.drawString(14.5 * cm, 24.25 * cm, str(ring))
+
+    def calculate_canvas_coordinates_from_competitor_index(self, competitor_count: int, competitor_index: int):
+        ''' calculates the canvas coordinates (physical x,s) for a competitor based on how many competitors there are
+         and what this competitor place in at list '''
+        # print(competitor_index)
+        column, row = BPM.calculate_bracket_position_from_competitor_index(competitor_count, competitor_index)
+        # print('backet coordinate: ', column, row)
+        if column == 1:
+            x_coordinate, y_coordinate = self.get_canvas_coord_for_nth_competitor_in_column1(row - 1)
+        else:
+            x_coordinate, y_coordinate = self.get_canvas_coord_for_nth_competitor_in_column2(row - 1)
+        x_coordinate = x_coordinate * cm
+        y_coordinate = y_coordinate * cm
+        # print('canvas coordinate: ', x_coordinate, y_coordinate)
+        return x_coordinate, y_coordinate
+
+    def draw_competitors_on_tree(self, competitors: Competitors) -> object:
+        ''' draw the competitors on the tree '''
+        # print(competitors)
+
+        competitor_count = competitors.get_number_of_competitors()
+        if competitor_count > 8:
+            print("*** Something is wrong! we have {} competitors for an 8 person tree".format(competitor_count))
+            competitor_count = 8
+        i = 0
+        for index, competitor in competitors.iterrows():
+            name = competitor['First_Name'] + ' ' + competitor['Last_Name']
+            # print('\n' + name)
+            px, py = self.calculate_canvas_coordinates_from_competitor_index(competitor_count, i)
+            self._c.drawString(px, py, name)
+            i = i + 1
+            # tbd - put the assertion back and remove the condition after you have a 16 person tree running
+            # assert i < 8,  "Should be no more than 8 competitors on an 8 person tree"
+            if i > 7:
+                break
+
+    def add_page_with_competitors_on_tree(self, ring: int, event_time: str, event_title: str, ranks,
+                                          competitors: Competitors) -> object:
+        ''' adds a compleate page with a tree and the competitors '''
+        # lay down the template
+        self.draw_static_template()
+
+        # lay down the header info
+        self.draw_header_info_on_tree(ring, event_time, event_title, ranks)
+
+        # draw the competitors onto the tree
+        self.draw_competitors_on_tree(competitors)
+
+        # close the tree - causes the page to be written
+        self.close()
+
+
+if __name__ == '__main__':
+    ''' Very simple test try to create a tree and check that the file exists '''
+    c = canvas.Canvas("16PersonTree.pdf", pagesize=letter)  # defaults to 72 pointer per inch
+    tree = SixteenCompetitorTree(c)
+    tree.draw_static_template()
+    tree.close()
+    c.save()
+    import os
+
+    if os.path.exists("16PersonTree.pdf"):
+        print("It worked")
---
+++ reporting/sparring_tree/sixteen_competitor_tree_test.py
@@ -0,0 +1,251 @@
+'''
+This module tests the sixteen_compettitor_tree module
+to run this test go to the main directory and run
+nosetests --with-coverage
+'''''
+
+import unittest
+import os
+from reportlab.pdfgen import canvas
+from reporting.sparring_tree.sixteen_competitor_tree import SixteenCompetitorTree
+from reporting.sparring_tree.competitors import Competitors
+
+REMOVE_TEST_FILES = False
+
+# from eight_competitor_tree import SixteenCompetitorTree
+# import eight_competitor_tree
+
+# data for the tests
+TEST_DATA_COLUMNS = ['index', 'First_Name', 'Last_Name', 'Gender', 'Dojo', 'Age', 'Rank', 'Feet', 'Inches', 'Height',
+                     'Weight', 'BMI', 'Events', 'Weapons', 'hitcount']
+
+TEST_DATA = [(1, 'Katie', 'Coleson', 'Female', 'CO- Parker', 12, 'White', 4, 0, '4', 65, 161,
+              '2 Events - Forms & Sparring ($75)', 'Weapons ($35)', 0),
+             (2, 'Lucas', 'May', 'Male', 'CO- Cheyenne Mountain', 10, 'Yellow', 4, 3, '4 ft. 3 in.', 52, 154,
+              '2 Events - Forms & Sparring ($75)', 'None', 0),
+             (3, 'Jake', 'Coleson', 'Male', 'CO- Cheyenne Mountain', 10, 'Yellow', 4, 0, '4', 60, 156,
+              '2 Events - Forms & Sparring ($75)', 'Weapons ($35)', 0),
+             (4, 'Allen', 'Whitaker', 'Male', 'CO- Arvada', 10, 'Yellow', 4, 0, '4', 55, 151,
+              '2 Events - Forms & Sparring ($75)', 'Weapons ($35)', 0),
+             (5, 'Bill', 'Kable', 'Male', 'CO- Cheyenne Mountain', 10, 'Yellow', 4, 1, '4', 63, 161,
+              '2 Events - Forms & Sparring ($75)', 'Weapons ($35)', 0)]
+
+SIXTEEN_NAMES = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+                 "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen"]
+
+
+class TestSixteenCompetitorTree(unittest.TestCase):
+    ''' class to test the SixteenCompettitorTree code'''
+
+    def setUp(self):
+        ''' setup code for the tests'''
+        return
+
+    def test_creating_a_file(self):
+        ''' simple test to make sure we can create a file with the template code in it'''
+        test_file_name = "16PersonTree_Create_File.pdf"
+        test_canvas = canvas.Canvas(test_file_name)
+        tree = SixteenCompetitorTree(test_canvas)
+        tree.draw_static_template()
+        tree.close()
+        test_canvas.save()
+
+        # test to see if the PDF file was created
+        self.assertTrue(os.path.exists(test_file_name))
+        if REMOVE_TEST_FILES:
+            os.remove(test_file_name)
+
+    def test_creating_a_file_with_two_tree(self):
+        ''' tests that we can create a PDF with multiple sparring trees in it'''
+        test_file_name = "16PersonTree_Two_Trees.pdf"
+        test_canvas = canvas.Canvas(test_file_name)
+        tree = SixteenCompetitorTree(test_canvas)
+
+        # draw first tree
+        tree.draw_static_template()
+        tree.close()
+
+        # draw second tree
+        tree.draw_static_template()
+        tree.close()
+
+        self.assertTrue(test_canvas.__dict__['_pageNumber'] == 3)  # Make sure we have 2 pages
+        test_canvas.save()
+        if REMOVE_TEST_FILES:
+            os.remove(test_file_name)
+
+    def test_initialize_text_coordinates(self):
+        ''' test to make sure the text coordinates are initialized '''
+        test_file_name = "16PersonTree_Init_Text_Coords.pdf"
+        test_canvas = canvas.Canvas(test_file_name)
+        tree = SixteenCompetitorTree(test_canvas)
+        self.assertTrue(len(tree._first_column_text_coordinates) == 16)
+        self.assertTrue(len(tree._second_column_text_coordinates) == 8)
+
+        # draw the tree just for the heck of it
+        tree.draw_static_template()
+        tree.close()
+        test_canvas.save()
+        # test to see if the PDF file was created
+        self.assertTrue(os.path.exists(test_file_name))
+        if REMOVE_TEST_FILES:
+            os.remove(test_file_name)
+
+    def draw_n_names_on_tree(self, names: list, test_file_name: str):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        print("\nDrawing {} ".format(test_file_name))
+        # setup a 16 person tree
+        test_canvas = canvas.Canvas(test_file_name)
+        tree = SixteenCompetitorTree(test_canvas)
+        tree.draw_static_template()
+
+        competitor_count = len(names)
+        i = 0
+        for i in range(competitor_count):
+            # print('\n' + names[i])
+            px, py = tree.calculate_canvas_coordinates_from_competitor_index(competitor_count, i)
+            test_canvas.drawString(px, py, names[i])
+
+        # draw the tree just for the heck of it
+        tree.draw_static_template()
+        tree.close()
+        test_canvas.save()
+        # test to see if the PDF file was created
+        self.assertTrue(os.path.exists(test_file_name))
+        if REMOVE_TEST_FILES:
+            os.remove(test_file_name)
+
+    def test_draw_16_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES
+        self.draw_n_names_on_tree(names, "16PeopleOn16PersonTree.pdf")
+
+    def test_draw_15_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES[0:15]
+        self.draw_n_names_on_tree(names, "15PeopleOn16PersonTree.pdf")
+
+    def test_draw_14_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES[0:14]
+        self.draw_n_names_on_tree(names, "14PeopleOn16PersonTree.pdf")
+
+    def test_draw_13_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES[0:13]
+        self.draw_n_names_on_tree(names, "13PeopleOn16PersonTree.pdf")
+
+    def test_draw_12_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES[0:12]
+        self.draw_n_names_on_tree(names, "12PeopleOn16PersonTree.pdf")
+
+    def test_draw_11_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES[0:11]
+        self.draw_n_names_on_tree(names, "11PeopleOn16PersonTree.pdf")
+
+    def test_draw_10_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES[0:10]
+        self.draw_n_names_on_tree(names, "10PeopleOn16PersonTree.pdf")
+
+    def test_draw_9_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES[0:9]
+        self.draw_n_names_on_tree(names, "9PeopleOn16PersonTree.pdf")
+
+    def test_draw_8_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES[0:8]
+        self.draw_n_names_on_tree(names, "8PeopleOn16PersonTree.pdf")
+
+    def test_draw_7_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES[0:7]
+        self.draw_n_names_on_tree(names, "7PeopleOn16PersonTree.pdf")
+
+    def test_draw_6_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES[0:6]
+        self.draw_n_names_on_tree(names, "6PeopleOn16PersonTree.pdf")
+
+    def test_draw_5_names_on_tree(self):
+        names = SIXTEEN_NAMES[0:5]
+        self.draw_n_names_on_tree(names, "5PeopleOn16PersonTree.pdf")
+
+    def test_draw_4_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES[0:4]
+        self.draw_n_names_on_tree(names, "4PeopleOn16PersonTree.pdf")
+
+    def test_draw_3_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES[0:3]
+        self.draw_n_names_on_tree(names, "3PeopleOn16PersonTree.pdf")
+
+    def test_draw_2_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES[0:2]
+        self.draw_n_names_on_tree(names, "2PeopleOn16PersonTree.pdf")
+
+    def test_draw_1_names_on_tree(self):
+        ''' tests to make sure the names in a 16 person tree get put in the right place'''
+        names = SIXTEEN_NAMES[0:1]
+        self.draw_n_names_on_tree(names, "1PeopleOn16PersonTree.pdf")
+
+    def test_draw_competitors_on_tree(self):
+        ''' tests that we each compettitor gets assigned physical coordinates '''
+        # create compettitors from our test data
+        # get the first and second column test coordinates from the tree
+        # run the algorythm to map compettitors into the tree
+        # description of how to add a new column to an existing dataframe https://www.geeksforgeeks.org/adding-new-column-to-existing-dataframe-in-pandas/
+        the_competitors = Competitors(TEST_DATA,
+                                      columns=TEST_DATA_COLUMNS)  # create a list of competitors from the test data above
+
+        # setup a 16 person tree
+        test_file_name = "16PersonTree_from_competitors.pdf"
+        test_canvas = canvas.Canvas(test_file_name)
+        tree = SixteenCompetitorTree(test_canvas)
+        tree.draw_static_template()
+
+        # draw the competitors onto the tree
+        tree.draw_competitors_on_tree(the_competitors)
+
+        # close out the 16 person tree
+        tree.close()
+        test_canvas.save()
+
+        # test to see if the PDF file was created
+        self.assertTrue(os.path.exists(test_file_name))
+        if REMOVE_TEST_FILES:
+            os.remove(test_file_name)
+
+    def test_add_page_with_competitors_on_tree(self):
+        ''' tests that we each compettitor gets assigned physical coordinates '''
+        # create compettitors from our test data
+        # get the first and second column test coordinates from the tree
+        # run the algorythm to map compettitors into the tree
+        # description of how to add a new column to an existing dataframe https://www.geeksforgeeks.org/adding-new-column-to-existing-dataframe-in-pandas/
+        the_competitors = Competitors(TEST_DATA,
+                                      columns=TEST_DATA_COLUMNS)  # create a list of competitors from the test data above
+
+        # setup a 16 person tree
+        test_file_name = "16PersonTree_full_page.pdf"
+        test_canvas = canvas.Canvas(test_file_name)
+        tree = SixteenCompetitorTree(test_canvas)
+
+        # draw the competitors onto the tree
+        tree.add_page_with_competitors_on_tree(1, "2:00", "Sr. Mens Sparring", "Black", the_competitors)
+
+        # save the file
+        test_canvas.save()
+
+        # test to see if the PDF file was created
+        self.assertTrue(os.path.exists(test_file_name))
+        if REMOVE_TEST_FILES:
+            os.remove(test_file_name)
+
+
+if __name__ == '__main__':
+    unittest.main()

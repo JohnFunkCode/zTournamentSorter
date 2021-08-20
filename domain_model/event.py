2889935cf0dfb0903cd169ee80c435338407ab78
JohnFunkCode <email@johnfunk.com>
2021-08-20 14:40:15 -0600

    updates to Event and Rank

--- domain_model/event.py
+++ domain_model/event.py
@@ -6,16 +6,19 @@ class Event():
     name = None
     time = None
     age_range_filter = None
+    ring_numbers = None
     rings = None
 
 
-    def __init__(self, name,time, age_range):
+    def __init__(self, name,time, age_range, ring_numbers):
         self.name=name
         self.time =time
         self. age_range_filter=age_range
+        self.ring_numbers=ring_numbers
 
+    def add_ring(self,ranks,):
 
 if __name__ == '__main__':
     ''' Very simple test try to create an event '''
-    e=Event('Kids Kata', '9:00', ['White'])
-    print(e.name,e.time,e.age_range_filter)
+    e=Event('Kids Kata', '9:00', ['4-6'],[1,2,3,4,5,6,7,8])
+    print(e.name,e.time,e.age_range_filter,e.ring_numbers)
---
+++ domain_model/rank.py
@@ -0,0 +1,10 @@
+import enum
+
+@enum.unique
+class Rank(Enum):
+    white='White'
+    yellow='Yellow'
+    orange='Orange'
+    purple='Purple'
+    green='Green'
+    green_stripe='Green'

c01bbbcafad92a30bb9d74f84dff59da04243353
John Funk <email@johnfunk.com>
2023-01-04 21:24:41 -0700

    GUI moves to next/previous error Also added revalidation button, and code to dump the data to the error panel for debugging.

--- GUI/datavalidation/data_validation_controller.py
+++ GUI/datavalidation/data_validation_controller.py
@@ -1,3 +1,4 @@
+import time
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
@@ -16,6 +17,10 @@ class DataValidationController():
         super().__init__()
         self.app_container = app_container
         self.data_validation_view = GUI.datavalidation.data_validation_view.DataValidationView(app_container,self)
+        self.input_error_list = input_errors.InputErrors()
+        self.error_cursor = 0
+        self.errorLogFileName=''
+
 
     def hide_view(self):
         self.data_validation_view.hide_view()
@@ -26,8 +31,8 @@ class DataValidationController():
     def load_tournament_file(self):
         filename = filedialog.askopenfilename(title="Select the file with the tournament data",filetypes=[("csv","*.csv")])
 
-        errorLogFileName = filename[0:len(filename) - 4] + "-Error.txt"
-        errorLogFile = open(errorLogFileName, "w")
+        self.errorLogFileName = filename[0:len(filename) - 4] + "-Error.txt"
+        errorLogFile = open(self.errorLogFileName, "w")
 
         cleaninput.clean_unicode_from_file(filename, errorLogFile)
 
@@ -35,24 +40,73 @@ class DataValidationController():
         r = RN.RenameColumns(filename)
         r.rename_all_columns()
         renamed_df = r.get_dataframe_copy()
-        input_error_list = input_errors.InputErrors()
-        clean_df,error_count=cleaninput.clean_all_input_errors(renamed_df, errorLogFile, input_error_list)
+        # self.input_error_list = input_errors.InputErrors()
+        clean_df,error_count=cleaninput.clean_all_input_errors(renamed_df, errorLogFile, self.input_error_list)
         self.app_container.database =clean_df
         self.data_validation_view.update_table()
 
-        print(f'Input Errors:{input_error_list.error_list}')
-        for i in range(len(input_error_list.error_list)):
-            if input_error_list.error_list[i][1]=='Age':
-                self.data_validation_view.highlight_age_error(input_error_list.error_list[i][0]+1)
-            if input_error_list.error_list[i][1]=='Height':
-                self.data_validation_view.highlight_weight_error(input_error_list.error_list[i][0]+1)
-            if input_error_list.error_list[i][1]=='Weight':
-                self.data_validation_view.highlight_weight_error(input_error_list.error_list[i][0]+1)
-            if input_error_list.error_list[i][1] == 'Rank':
-                self.data_validation_view.highlight_rank_error(input_error_list.error_list[i][0]+1)
+        print(f'Input Errors:{self.input_error_list.error_list}')
+        for i in range(len(self.input_error_list.error_list)):
+            if self.input_error_list.error_list[i][1]=='Age':
+                self.data_validation_view.highlight_age_error(self.input_error_list.error_list[i][0]+1)
+            if self.input_error_list.error_list[i][1]=='Height':
+                self.data_validation_view.highlight_weight_error(self.input_error_list.error_list[i][0]+1)
+            if self.input_error_list.error_list[i][1]=='Weight':
+                self.data_validation_view.highlight_weight_error(self.input_error_list.error_list[i][0]+1)
+            if self.input_error_list.error_list[i][1] == 'Rank':
+                self.data_validation_view.highlight_rank_error(self.input_error_list.error_list[i][0]+1)
+        self.input_error_list.error_list.sort()
 
+        errorLogFile.close()
         self.data_validation_view.table.show()
         self.data_validation_view.show_view()
+
+        # TBD figure out how to goto the first error the first time the table is loaded - it appears the table isn't rendered yet.
+        # row= self.input_error_list.error_list[self.error_cursor][0] + 1
+        # column = self.input_error_list.error_list[self.error_cursor][1]
+        # self.data_validation_view.goto_row_column(row,column)
+
+
+
+    def validate_data(self):
+        errorLogFile = open(self.errorLogFileName, "a")
+        self.input_error_list = []
+        self.input_error_list = input_errors.InputErrors()
+        self.data_validation_view.reset_color()
+
+        df=self.app_container.database
+        clean_df,error_count=cleaninput.clean_all_input_errors(df, errorLogFile, self.input_error_list)
+        self.app_container.database =clean_df
+        self.data_validation_view.update_table()
+
+        self.input_error_list.error_list.sort()
+        print(f'{time.strftime("%X")} Input Errors:{self.input_error_list.error_list}')
+        for i in range(len(self.input_error_list.error_list)):
+            if self.input_error_list.error_list[i][1]=='Age':
+                self.data_validation_view.highlight_age_error(self.input_error_list.error_list[i][0]+1)
+                # print(f'Age:{time.strftime("%X")}')
+
+            if self.input_error_list.error_list[i][1]=='Height':
+                self.data_validation_view.highlight_height_error(self.input_error_list.error_list[i][0]+1)
+                # print(f'Height:{time.strftime("%X")}')
+
+            if self.input_error_list.error_list[i][1]=='Weight':
+                self.data_validation_view.highlight_weight_error(self.input_error_list.error_list[i][0]+1)
+                # print(f'Weight:{time.strftime("%X")}')
+
+            if self.input_error_list.error_list[i][1] == 'Rank':
+                self.data_validation_view.highlight_rank_error(self.input_error_list.error_list[i][0]+1)
+                # print(f'Rank:{time.strftime("%X")}')
+
+        print(time.strftime("%X"))
+
+        errorLogFile.close()
+        self.data_validation_view.table.redraw()
+        self.data_validation_view.table.show()
+        self.data_validation_view.show_view()
+        print(time.strftime("%X"))
+
+
 
     def load_division_file(self):
         filename = filedialog.askopenfilename(title="Select the file with the division data",filetypes=[("csv","*.csv"),("excel","*.xls")])
@@ -61,7 +115,38 @@ class DataValidationController():
         self.data_validation_view.table.show()
         self.data_validation_view.show_view()
 
+
+    def previous_error(self):
+        #self.validate_data()
+        if self.error_cursor>0:
+            self.error_cursor-=1
+        row= self.input_error_list.error_list[self.error_cursor][0] + 1
+        column = self.input_error_list.error_list[self.error_cursor][1]
+        self.data_validation_view.goto_row_column(row,column)
+        #showinfo(title='Info', message="Previous error")
+
+    def next_error(self):
+        if self.error_cursor<len(self.input_error_list.error_list)-1:
+            self.error_cursor+=1
+        row= self.input_error_list.error_list[self.error_cursor][0] + 1
+        column = self.input_error_list.error_list[self.error_cursor][1]
+        self.data_validation_view.goto_row_column(row,column)
+        #showinfo(title='Info', message="Next error")
+
+    def recheck_data(self):
+        self.validate_data()
+        self.error_cursor=0
+        row= self.input_error_list.error_list[self.error_cursor][0] + 1
+        column = self.input_error_list.error_list[self.error_cursor][1]
+        self.data_validation_view.goto_row_column(row,column)
+
+
     def process_data(self):
+        showinfo(title='Info', message="Start processing data")
+
+    def process_data(self):
+        self.data_validation_view.error_log.delete("1.0",tk.END)
+        self.data_validation_view.error_log.insert(tk.INSERT,self.app_container.database.to_string())
         showinfo(title='Info', message="Start processing data")
 
     def test_one(self):
--- GUI/datavalidation/data_validation_view.py
+++ GUI/datavalidation/data_validation_view.py
@@ -15,29 +15,42 @@ class DataValidationView(ttk.Frame):
         # add padding to the frame and show it
         self.grid(**options)
 
-        # data table label
-        self.error_log_label = ttk.Label(self, text='Data Table')
-        self.error_log_label.grid(column=0, row=0, sticky=tk.W, **options)
+        # # data table label
+        # self.error_log_label = ttk.Label(self, text='Data Table')
+        # self.error_log_label.grid(column=0, row=0, sticky=tk.W, **options)
 
         # pandas table
         self.pandas_table_frame = ttk.Frame(self,borderwidth=2,relief='sunken')
         self.table = Table(parent=self.pandas_table_frame, model=TableModel(app_container.database),
                            showtoolbar=False, showstatusbar=False, enable_menus=False, width=1550, height=400)
-        self.pandas_table_frame.grid(column=0,row=1,columnspan=3,**options)
+        self.pandas_table_frame.grid(row=1,column=0,rowspan=3, columnspan=3,**options)
         self.table.redraw()
+
+        self.previous_button = ttk.Button(self, text='Previous')
+        self.previous_button['command'] = self.handle_previous_button
+        self.previous_button.grid(row=1, column=3,sticky=tk.E, **options)
+
+        self.previous_button = ttk.Button(self, text='Next')
+        self.previous_button['command'] = self.handle_next_button
+        self.previous_button.grid( row=2, column=3, sticky=tk.E, **options)
+
+        self.previous_button = ttk.Button(self, text='Re-Check')
+        self.previous_button['command'] = self.handle_recheck_button
+        self.previous_button.grid(row=3, column=3, sticky=tk.E, **options)
+
 
         # error log label
         self.error_log_label = ttk.Label(self, text='Data Errors')
-        self.error_log_label.grid(column=0, row=2, sticky=tk.W, **options)
+        self.error_log_label.grid(row=4, column=0, sticky=tk.W, **options)
 
         # error log textbox
-        self.error_log = tk.scrolledtext.ScrolledText(self,width=200)
-        self.error_log.grid(column=0, row=3, columnspan=3, **options)
+        self.error_log = tk.scrolledtext.ScrolledText(self,width=200,wrap="none")
+        self.error_log.grid(row=5, column=0, columnspan=3, **options)
 
         # button 1
-        self.button_1 = ttk.Button(self, text='Process Data')
-        self.button_1['command'] = self.process_data
-        self.button_1.grid(column=3, row=4, sticky=tk.E, **options)
+        self.process_data_button = ttk.Button(self, text='Process Data')
+        self.process_data_button['command'] = self.process_data
+        self.process_data_button.grid(row=6, column=3,sticky=tk.E, **options)
 
     def show_view(self):
         self.grid()
@@ -45,8 +58,18 @@ class DataValidationView(ttk.Frame):
     def hide_view(self):
         self.grid_forget()
 
+    def handle_previous_button(self):
+        self.controller.previous_error()
+
+    def handle_next_button(self):
+        self.controller.next_error()
+
+    def handle_recheck_button(self):
+        self.controller.recheck_data()
+
+
     def process_data(self):
-        self.controller.process_data() ;
+        self.controller.process_data()
 
     def update_table(self):
         self.table.updateModel(TableModel(self.app_container.database))
@@ -56,14 +79,14 @@ class DataValidationView(ttk.Frame):
         age_column = self.table.model.df.columns.get_loc('Age')
         age_columns = range(age_column,age_column+1)
         self.table.setRowColors(rows=error_rows,clr='red',cols=age_columns)
-        self.table.redraw()
+        #self.table.redraw()
 
     def highlight_weight_error(self, row_with_error):
         error_rows = range(row_with_error-1,row_with_error)
         age_column = self.table.model.df.columns.get_loc('Weight')
         age_columns = range(age_column,age_column+1)
         self.table.setRowColors(rows=error_rows,clr='red',cols=age_columns)
-        self.table.redraw()
+        #self.table.redraw()
 
 
     def highlight_height_error(self, row_with_error):
@@ -71,14 +94,19 @@ class DataValidationView(ttk.Frame):
         age_column = self.table.model.df.columns.get_loc('Height')
         age_columns = range(age_column,age_column+1)
         self.table.setRowColors(rows=error_rows,clr='red',cols=age_columns)
-        self.table.redraw()
+        #self.table.redraw()
 
     def highlight_rank_error(self, row_with_error):
         error_rows = range(row_with_error-1,row_with_error)
         age_column = self.table.model.df.columns.get_loc('Rank')
         age_columns = range(age_column,age_column+1)
         self.table.setRowColors(rows=error_rows,clr='red',cols=age_columns)
-        self.table.redraw()
+        #self.table.redraw()
+
+    def reset_color(self):
+        all_rows = range(0,self.table.model.df.shape[0]-1)
+        #all_columns = range(0,self.table.model.df.shape[1]-1)
+        self.table.setRowColors(rows=all_rows,clr='white',cols='all')
 
 
     def goto_row_column(self,row, column_title):
